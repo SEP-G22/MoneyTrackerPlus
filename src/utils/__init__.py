2201e4eb4abf2afcf36f6f31a68defa1535bf98a
PuiPui32071 <PuiPui.32071@gmail.com>
2025-03-19 21:57:12 +0800

    Remove cloud related functions again

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,2 @@
-from .account_book_utils import get_local_books, get_cloud_books, get_account_book
+from .account_book_utils import get_local_books, get_account_book
 from .chart_generator import generate_pie_chart, generate_line_chart, generate_bar_chart
